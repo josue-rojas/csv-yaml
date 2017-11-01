253dd973252600a2537abf5f3cc2f8afc6145fe8
josue rojas <josuerojas.rojas@gmail.com>
2017-11-01 15:59:30 -0400

    added function for url csv to yaml

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # csv-yaml
-a script to convert csv to yaml files using PyYaml
+a script to convert csv to yaml files using PyYaml for use in Jekyll templating among other things
+
+### Need
+- [PyYaml](http://pyyaml.org/wiki/PyYAMLDocumentation)
+- I am using python 2.7.....
+- I am also using mac............
--- convert.py
+++ convert.py
@@ -1,25 +1,32 @@
 import yaml
 import os
 import csv
-import json
-# import urllib2 # used for sheets online csv
-
-# a = {'name': 'Silenthand Olleander', 'race': 'Human'}
-# print yaml.dump({'name': 'Silenthand Olleander', 'race': 'Human'})
+import urllib2 # i used for sheets online csv
+# https://stackoverflow.com/questions/16283799/how-to-read-a-csv-file-from-a-url-with-python
 
 root = os.getcwd()
 
+#
+# takes a csvFile name and output file name/path
+def csvToYaml(csvFile, output):
+    stream = file(output, 'w')
+    csvOpen = csv.reader(csvFile)
+    keys = next(csvOpen)
+    for row in csvOpen:
+        yaml.dump([dict(zip(keys, row))], stream, default_flow_style=False)
 
-for f in os.listdir(root):
-    if f.endswith('.csv'):
-        csvFile = os.path.join(root, f)
-        with open(csvFile, 'rb') as csvfile:
-            stream = file(f.replace('.csv','.yml'), 'w')
-            csvOpen = csv.reader(csvfile)
-            keys = next(csvOpen)
-            # data = []
-            for row in csvOpen:
-                # row = dict(zip(keys, row))
-                # data.extend([dict(zip(keys, row))])
-                # print data
-                yaml.dump([dict(zip(keys, row))], stream, default_flow_style=False)
+def urlCSV(url, output=None):
+    csvFile = urllib2.urlopen(url)
+    output = output if output else root+'/'+(url.split('/')[-1].replace('.csv','.yml'))
+    csvToYaml(csvFile, output)
+
+def localCSV():
+    for f in os.listdir(root):
+        if f.endswith('.csv'):
+            csvFile = os.path.join(root, f)
+            with open(csvFile, 'rb') as csvfile:
+                output = f.replace('.csv','.yml')
+                csvToYaml(csvfile, output)
+
+
+urlCSV('http://winterolympicsmedals.com/medals.csv')
