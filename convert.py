adb855d4c396056025d94ae3269646abe0a954c3
josue rojas <josuerojas.rojas@gmail.com>
2017-11-02 17:52:23 -0400

    closes #1, closes #2, closes #3, closes #4, needs testing and more print -h

--- convert.py
+++ convert.py
@@ -1,12 +1,22 @@
 import yaml
 import os
 import csv
+<<<<<<< HEAD
 import urllib2 # i used for online csv
+=======
+import getopt
+import sys
+import urllib2 # i used for sheets online csv
+>>>>>>> closes #1, closes #2, closes #3, closes #4, needs testing and more print -h
 # https://stackoverflow.com/questions/16283799/how-to-read-a-csv-file-from-a-url-with-python
 
 root = os.getcwd()
 
+<<<<<<< HEAD
 #
+=======
+
+>>>>>>> closes #1, closes #2, closes #3, closes #4, needs testing and more print -h
 # takes a csvFile name and output file name/path
 def csvToYaml(csvFile, output):
     stream = file(output, 'w')
@@ -27,3 +37,45 @@ def localCSV():
             with open(csvFile, 'rb') as csvfile:
                 output = f.replace('.csv','.yml')
                 csvToYaml(csvfile, output)
+<<<<<<< HEAD
+=======
+
+def usage():
+    # write instructions....
+    print 'help is ummm.....'
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hi:o:u', ['help', 'input=','output=', 'url'])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    csvFile = None
+    output = None
+    url = False
+    if len(opts) == 0:
+        localCSV()
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            sys.exit()
+        elif o in ('-i', '--input'):
+            csvFile = a
+        elif o in ('-o', '--output'):
+            output = a
+        elif o in ('-u', '--url'):
+            url = True
+        else:
+            print 'unhandled option'
+    if url:
+        urlCSV(csvFile, output)
+    output = output if output else root+'/'+(csvFile.split('/')[-1].replace('.csv','.yml'))
+    with open(csvFile, 'rb') as csvFile:
+        csvToYaml(csvFile, output)
+
+
+if __name__ == "__main__":
+    main()
+>>>>>>> closes #1, closes #2, closes #3, closes #4, needs testing and more print -h
