e22314f5b489d3681b3ce84ab920b23edfd0d78c
josue rojas <josuerojas.rojas@gmail.com>
2017-11-03 19:24:17 -0400

    added usage; seperate some functions;

--- convert.py
+++ convert.py
@@ -16,29 +16,35 @@ def csvToYaml(csvFile, output):
     for row in csvOpen:
         yaml.dump([dict(zip(keys, row))], stream, default_flow_style=False)
 
+# converts single url file
 def urlCSV(url, output=None):
     csvFile = urllib2.urlopen(url)
     output = output if output else root+'/'+(url.split('/')[-1].replace('.csv','.yml'))
     csvToYaml(csvFile, output)
 
-def localCSV():
-    for f in os.listdir(root):
+# converts all csv file in this folder
+def localCSV(folder=root):
+    for f in os.listdir(folder):
         if f.endswith('.csv'):
             csvFile = os.path.join(root, f)
-            with open(csvFile, 'rb') as csvfile:
-                output = f.replace('.csv','.yml')
-                csvToYaml(csvfile, output)
+            output = f.replace('.csv','.yml')
+            singleCSV(csvFile, output)
 
+# converts only one csv file
+def singleCSV(csvFile, output=None):
+    output = output if output else root+'/'+(csvFile.split('/')[-1].replace('.csv','.yml'))
+    with open(csvFile, 'rb') as csvFile:
+        csvToYaml(csvFile, output)
+
+# print -h --help
 def usage():
-    # write instructions....
-    print 'help is ummm.....'
+    print '\nUsage:\n-i --input: path/link of file (if link use url flag)\n-o --output: path name of output, if left out will convert in this folder using its name as output\n-u --url: url flag indicating input is a url and should be treated as such\n-h --help: print this/help stuff.....'
 
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'hi:o:u', ['help', 'input=','output=', 'url'])
     except getopt.GetoptError as err:
-        # print help information and exit:
-        print str(err)  # will print something like "option -a not recognized"
+        print str(err)
         usage()
         sys.exit(2)
     csvFile = None
@@ -46,6 +52,7 @@ def main():
     url = False
     if len(opts) == 0:
         localCSV()
+        exit()
     for o, a in opts:
         if o in ('-h', '--help'):
             usage()
@@ -60,9 +67,8 @@ def main():
             print 'unhandled option'
     if url:
         urlCSV(csvFile, output)
-    output = output if output else root+'/'+(csvFile.split('/')[-1].replace('.csv','.yml'))
-    with open(csvFile, 'rb') as csvFile:
-        csvToYaml(csvFile, output)
+        exit()
+    singleCSV(csvFile, output)
 
 
 if __name__ == "__main__":
